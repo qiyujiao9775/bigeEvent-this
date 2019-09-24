cfa4a9ac5718658944497fe5120b118b301002c5
jiao~ <jiao~>
2019-09-24 16:55:15 +0800

    编辑文章分类  即修改

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -1,7 +1,7 @@
 var category = {
     // 查看
-    show: function(callBack) {
-        $.get(URLIST.category_show, function(res) {
+    show: function (callBack) {
+        $.get(URLIST.category_show, function (res) {
             callBack(res);
         });
     },
@@ -11,14 +11,28 @@ var category = {
      * @param {*} callBack   回调函数
      */
     //添加文章
-    add: function(name,slug,callBack) {
-        $.post(URLIST.category_add,{name:name,slug:slug} ,function(res) {
+    add: function (name, slug, callBack) {
+        $.post(URLIST.category_add, {
+            name: name,
+            slug: slug
+        }, function (res) {
             callBack(res);
         });
     },
-    del:function(id){
-        $.post(URLIST.category_del,{id:id} ,function(res) {
-        callBack(res);
-    });
+    del: function (id, callBack) {
+        $.post(URLIST.category_del, {
+            id: id
+        }, function (res) {
+            callBack(res);
+        });
+    },
+    edit: function (id, name, slug, callBack) {
+        $.post(URLIST.category_edit, {
+            id: id,
+            name: name,
+            slug: slug
+        }, function (res) {
+            callBack(res);
+        });
     }
 }
--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -6,7 +6,8 @@ var URLIST = {
 
   category_show:BASE_URL+"/admin/category_search",
   category_add:BASE_URL+"/admin/category_add",
-  category_del:BASE_URL+"/admin/category_delete"
+  category_del:BASE_URL+"/admin/category_delete",
+  category_edit:BASE_URL+"/admin/category_edit",
 
 
 };
