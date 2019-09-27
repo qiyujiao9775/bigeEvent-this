fef8530acbe93870282122717791515001b7fe6c
jiao~ <jiao~>
2019-09-27 20:32:44 +0800

    评论

---
+++ web_front/js/config.js
@@ -0,0 +1,16 @@
+var BASE_URL = 'http://localhost:8000';
+var URLIST = {
+  user_login: BASE_URL + '/admin/login',
+  user_logout: BASE_URL + '/admin/logout',
+  user_info: BASE_URL + '/admin/getuser',
+
+  category_show:BASE_URL+"/admin/category_search",
+  category_add:BASE_URL+"/admin/category_add",
+  category_del:BASE_URL+"/admin/category_delete",
+  category_edit:BASE_URL+"/admin/category_edit",
+
+  article_show: BASE_URL + '/admin/search',
+  article_del: BASE_URL + '/admin/article_delete',
+  article_add: BASE_URL + '/admin/article_publish',
+  article_edit: BASE_URL + '/admin/article_edit'
+};
